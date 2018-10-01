382c49670e0671ae3eed72330f5d0e6b4d91a67d
Peter Burkholder <peter.burkholder@gsa.gov>
2018-10-01 13:33:24 -0400

    Add add'l functions

--- rate-limit-demo/demo.sh
+++ rate-limit-demo/demo.sh
@@ -4,17 +4,42 @@ function deploy_node() {
     (cd node-app; cf push --random-route)
 }
 
-node_app_host=$(
-    cf curl /v2/apps/$(cf app hello-iife --guid)/summary | 
-     jq -r '.routes[0].host')
-node_app_domain=$(
-    cf curl /v2/apps/$(cf app hello-iife --guid)/summary | 
-     jq -r '.routes[0].domain.name')
+function setup_vars() {
+    node_app_host=$(
+        cf curl /v2/apps/$(cf app hello-iife --guid)/summary | 
+        jq -r '.routes[0].host')
+    node_app_domain=$(
+        cf curl /v2/apps/$(cf app hello-iife --guid)/summary | 
+        jq -r '.routes[0].domain.name')
+}
 
 function run_ab(){
     H=https://$node_app_host.$node_app_domain/
     curl $H
-    ab -n 100 -c 10 $H/
+    ab -n 120 -c 15 $H/
 }
 
+function deploy_ratelimiter() {
+    (cd ratelimit-service && cf push ratelimiter)
+    cf create-user-provided-service ratelimiter-service -r https://ratelimiter.app.cloud.gov
+    cf bind-route-service app.cloud.gov ratelimiter-service --hostname $node_app_host
+}
+
+function clean_up() {
+    cf delete -f hello-iife
+    cf unbind-route-service app.cloud.gov -f ratelimiter-service --hostname $node_app_host
+    cf delete-route app.cloud.gov --hostname ratelimiter -f
+    cf ds -f ratelimiter-service
+    cf delete -f ratelimiter
+    cf delete-orphaned-routes -f
+    
+
+}
+
+deploy_node
+setup_vars
 run_ab
+deploy_ratelimiter
+run_ab
+clean_up
+
